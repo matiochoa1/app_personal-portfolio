94bbfdaf2e4bc691bbf8a9e4fb80286b419a4a12
Matias <mattiochoax@gmail.com>
2025-04-18 19:57:48 +0200

    Updating CV

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -36,7 +36,7 @@ export default function AboutMe() {
 									type="button"
 									className="h-10 px-4 py-2 border border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
 									<a
-										href="/files/Matias_Ochoa_CV.pdf"
+										href="/files/Matias_Ochoa_CV_2025.pdf"
 										download
 										className="flex items-center">
 										<Download className="w-4 h-4 mr-2" />
